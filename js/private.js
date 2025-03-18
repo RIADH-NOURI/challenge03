822a44cc45ec1ea349b190c9bd24551c9a80c3fe
RIADH-NOURI <riadhnouri0502@gmail.com>
2025-03-18 18:31:31 +0100

    fix all problems

--- js/private.js
+++ js/private.js
@@ -2,7 +2,7 @@
     () => {
         const token = localStorage.getItem('token');
         if (!token) {
-            window.location.href = '/index.html';
+            window.location.href = './index.html';
         }
     }
 )();
--- js/public.js
+++ js/public.js
@@ -2,7 +2,7 @@
     () => {
         const token = localStorage.getItem('token')
         if (token) {
-            window.location.href = '../shorten.html'
+            window.location.href = './shorten.html'
         }
     }
 )()
